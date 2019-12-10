6cb6e1b4c7680b04ecdba571cd0c9bf5c6b10443
Steven Rojas <steven.rojas@akros.tech>
2019-12-10 16:57:28 -0600

    Added material-ui

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Auxiliar from "../../hoc/Auxiliar";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 const layout = props => (
   <Auxiliar>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Tailorift
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
     <main>{props.children}</main>
   </Auxiliar>
 );
